005bafc4cd9a0c39525bd8312cbb01f564b59ee6
Mario Nguyễn <53334967+marionguyen20@users.noreply.github.com>
2021-02-18 18:51:23 +0700

    Complte

---
+++ LoginForm/main.js
@@ -0,0 +1,20 @@
+//Select content of class .input
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc () {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc () {
+    let parent = this.parentNode.parentNode;
+    if (this.value == ''){
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus',focusFunc);
+    input.addEventListener('blur',blurFunc);
+})
+
