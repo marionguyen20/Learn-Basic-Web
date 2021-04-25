efd6b2e1cdcfabf43ed95fcddbaee8e70af767a0
Mario Nguyễn <53334967+marionguyen20@users.noreply.github.com>
2021-04-25 14:26:56 +0700

    Update img,video

--- ReactJs/travelpage/src/App.js
+++ ReactJs/travelpage/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import NavBar from './components/NavBar'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path = '/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ ReactJs/travelpage/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <>
+            <nav className = 'navbar'>
+                <div className = 'navbar-container'>
+                    <Link to ='/' className = 'navbar-logo'>
+                        TRVL <i className = "fab fa-typo3" />
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
+export default NavBar
